8fb8068d26b96277de6f1f1513bb4daa611b2a27
Gabriel Petzalt <gableonn@gmail.com>
2020-10-29 20:26:10 -0300

    Feature: Base structure.

--- src/main/java/com/example/booking/Equipment/EquipmentRepository.java
+++ src/main/java/com/example/booking/Equipment/EquipmentRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EquimentRepository extends JpaRepository<Repository, Integer> {
+public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
     
 }
