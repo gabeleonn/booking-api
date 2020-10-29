35b9a97ac25ced131acaf3c196fdddc943b18682
Gabriel Petzalt <gableonn@gmail.com>
2020-10-29 20:10:02 -0300

    Feature: Base structure.

--- src/main/java/com/example/booking/Equipment/EquipmentController.java
+++ src/main/java/com/example/booking/Equipment/EquipmentController.java
@@ -1,6 +1,11 @@
 package com.example.booking.Equipment;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +14,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class EquipmentController {
     
     @GetMapping
-    public String hello() {
-        return "Hello World!";
+    public static String findAll() {
+        return "Get many";
+    }
+
+    @GetMapping("{id}")
+    public static String findOne(@PathVariable(value = "id") Integer id) {
+        return String.format("Get one (%s)", Integer.toString(id));
+    }
+
+    @PostMapping
+    public static String create(@RequestBody Equipment newEquipment) {
+        return String.format("Adding new (%s)", newEquipment.getId());
+    }
+
+    @DeleteMapping("{id}")
+    public static String deleteOne(@PathVariable(value = "id") String id) {
+        return String.format("Deleting (%s)", id);
+    }
+
+    @PatchMapping("{id}")
+    public static String updateOne(@PathVariable(value = "id") String id) {
+        return String.format("Updating (%s)", id);
     }
 }
--- src/main/java/com/example/booking/Ticket/TicketController.java
+++ src/main/java/com/example/booking/Ticket/TicketController.java
@@ -1,5 +1,11 @@
 package com.example.booking.Ticket;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,4 +13,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RequestMapping("/tickets")
 public class TicketController {
     
+    @GetMapping
+    public static String findAll() {
+        return "Get many";
+    }
+
+    @GetMapping("{id}")
+    public static String findOne(@PathVariable(value = "id") Integer id) {
+        return String.format("Get one (%s)", Integer.toString(id));
+    }
+
+    @PostMapping
+    public static String create(@RequestBody Ticket newTicket) {
+        return String.format("Adding new (%s)", newTicket.getId());
+    }
+
+    @DeleteMapping("{id}")
+    public static String deleteOne(@PathVariable(value = "id") String id) {
+        return String.format("Deleting (%s)", id);
+    }
+
+    @PatchMapping("{id}")
+    public static String updateOne(@PathVariable(value = "id") String id) {
+        return String.format("Updating (%s)", id);
+    }
+
 }
